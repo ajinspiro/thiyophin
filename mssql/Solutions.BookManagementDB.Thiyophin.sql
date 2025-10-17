add30981564adb502eb0217603087fb492661c77
thiyophinj <thiyophin@gmail.com>
2025-10-17 13:04:48 +0530

    completed book management task

--- mssql/Solutions.BookManagementDB.Thiyophin.sql
+++ mssql/Solutions.BookManagementDB.Thiyophin.sql
@@ -31,3 +31,40 @@ SELECT Authors.Name as AuthorName, COUNT(Authors.Name) as NumberBooksWrote From 
 SELECT Authors.Name as AuthorName, COUNT(Authors.Name) as NumberBooksWrote From Books Join Authors on Books.AuthorId=Authors.Id Group By Authors.Name;
 Select AVG(Price) as AveragePriceAfter2010 from Books where Year(PublishedDate) > 2010;
 
+-- Task 6
+ALTER TABLE Books ADD Genre varchar(100);
+Update Books set Genre='Fiction' where Id=1;
+Update Books set Genre='Biography' where Id=2;
+Update Books set Genre='Adventure' where Id=3;
+Update Books set Genre='Fiction' where Id=4;
+Update Books set Genre='Education' where Id=5;
+Update Books set Genre='Mystery' where Id=6;
+
+-- Task 7
+ALTER TABLE Books ADD Rating decimal(3,2);
+Update Books set Rating=4.3 where Id=1;
+Update Books set Rating=4.8 where Id=2;
+Update Books set Rating=4.1 where Id=3;
+Update Books set Rating=3.9 where Id=4;
+Update Books set Rating=4.7 where Id=5;
+Update Books set Rating=4.5 where Id=6;
+
+-- Task 8
+SELECT Top(3) * from Books order by rating Desc;
+Select avg(Rating) as AverageRatingPerGenre, Genre from Books GROUP BY Genre;
+SELECT sum(Price) as TotalPriceOfBooksFromIndia from Books join Authors on Books.AuthorId=Authors.Id where Authors.Country='India';
+Select Title, Rating, IsAvailable from Books where IsAvailable=1 and Rating >= 4.5;
+
+
+-- Task 9
+ALTER TABLE Books ADD DiscountPercent decimal(5,2);
+UPDATE Books set DiscountPercent=15 where Price>500;
+UPDATE Books set DiscountPercent=5 where Price<=500;
+Alter TABLE Books ADD FinalPrice decimal(5,2);
+UPDATE Books set FinalPrice=(Price-(Price * DiscountPercent/100))
+
+-- Task 10
+Select Title, Price, DiscountPercent, FinalPrice from Books;
+Select sum(FinalPrice) as SumOfFinalPriceGroupedByGenre, avg(FinalPrice) as AvgOfFinalPriceGroupedByGenre from Books GROUP BY Genre;
+DELETE from Books where Rating<4.0 or IsAvailable=0;
+SELECT * from Books;
